98de45b2fe793905893dcb743284d832a702e6df
Dan Fingal-Surma <dfingal@gmail.com>
2015-03-03 01:23:25 -0800

    c++ impl

---
+++ envelopes.cc
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <random>
+#include <time.h>
+
+const int kNumTrials = 10000;
+const int kLowerPriorMax = 100;
+
+// Initialize random number generators.
+std::mt19937 gen(time(NULL));
+std::uniform_real_distribution<> value_distribution(0, kLowerPriorMax);
+
+// Runs a single trial where an envelope is chosen.  If the chosen envelope has
+// a value < cutoff, the function will switch envelopes, otherwise it will keep
+// the envelope it has chosen. Returns the value of the envelope it ultimately 
+// selects.
+double SingleTrial(int cutoff) {
+  double lower_value = value_distribution(gen);
+  double higher_value = 2 * lower_value;
+  if (gen() % 2 == 0) {
+    return lower_value >= cutoff ? lower_value : higher_value;
+  } else {
+    return higher_value >= cutoff ? higher_value : lower_value;
+  }
+}
+
+// Runs many trials at a given cutoff to approximate the expected value.
+double MultiTrial(int cutoff) {
+  double total = 0;
+  for (int i = 0; i < kNumTrials; ++i) {
+    total += SingleTrial(cutoff);
+  }  
+  return total / kNumTrials;
+}
+
+int main(int argc, char* argv[]) {
+  for (int cutoff = 0; cutoff < 2 * kLowerPriorMax; ++cutoff) {
+    printf("cutoff=%d, expected_value=%f\n", cutoff, MultiTrial(cutoff));
+  }
+  return 0;
+}
