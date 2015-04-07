4217fd683dc073df7c411ce1613c8f33b72df601
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-07 00:27:47 -0700

    Inline PRIOR_LOWER_MAX

--- envelopes_opt.py
+++ envelopes_opt.py
@@ -3,15 +3,13 @@
 import random
 
 NUM_TRIALS = 10000
-PRIOR_LOWER_MAX = 100
 		
 def multi_trial(cutoff):
   """Runs many trials at a given cutoff to approximate the expected value."""
   randfloat = random.random
-  prior_lower_max = PRIOR_LOWER_MAX
   total_result = 0
   for _ in xrange(NUM_TRIALS):
-    lower_value = randfloat() * prior_lower_max
+    lower_value = randfloat() * 100
     if randfloat() * 2 < 1:
       if lower_value >= cutoff:
         total_result = total_result + lower_value
@@ -28,5 +26,5 @@ def multi_trial(cutoff):
 
 if __name__ == '__main__':
   """Approximates the expected value for each integral cutoff value."""
-  for cutoff in xrange(2 * PRIOR_LOWER_MAX):
+  for cutoff in xrange(200):
     print "cutoff=%s, expected_value=%s" % (cutoff, multi_trial(cutoff))
