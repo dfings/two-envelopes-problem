0bc07851081e372e04913965d4b6fe6af1d03b98
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-15 23:01:55 -0700

    Remove unneeded parens

--- envelopes.ex
+++ envelopes.ex
@@ -12,31 +12,31 @@ defmodule Envelopes do
 
   # Returns the result of a single trial. We switch if the value is below the
   # cutoff.
-  def single_trial(cutoff) do
+  def single_trial cutoff do
     lower_value = :random.uniform() * @prior_lower_max
     higher_value = 2 * lower_value
-    case :random.uniform(2) do
+    case :random.uniform 2 do
       1 -> if lower_value >= cutoff do lower_value else higher_value end
       2 -> if higher_value >= cutoff do higher_value else lower_value end
     end
   end
 
   # Returns the total value of all of the trials for a given cutoff level.
-  def get_multi_trial_total(_, @num_trials, total) do total end
-  def get_multi_trial_total(cutoff, i, total) do
-    get_multi_trial_total(cutoff, i + 1, total + single_trial(cutoff))
+  def get_multi_trial_total _, @num_trials, total do total end
+  def get_multi_trial_total cutoff, i, total do
+    get_multi_trial_total cutoff, i + 1, total + (single_trial cutoff)
   end
 
   # Prints the expected value for each possible cutoff.
-  def cutoff_trials(2 * @prior_lower_max) do end
-  def cutoff_trials(cutoff) do
-    expected_value = get_multi_trial_total(cutoff, 0, 0) / @num_trials
-    IO.puts :io_lib.format("cutoff=~w, expected_value=~w", [cutoff, expected_value])
-    cutoff_trials(cutoff + 1)
+  def cutoff_trials (2 * @prior_lower_max) do end
+  def cutoff_trials cutoff do
+    expected_value = (get_multi_trial_total cutoff, 0, 0) / @num_trials
+    IO.puts (:io_lib.format "cutoff=~w, expected_value=~w", [cutoff, expected_value])
+    cutoff_trials cutoff + 1
   end
 
   def main do
-    :random.seed(:erlang.now)
-    cutoff_trials(0)
+    :random.seed :erlang.now
+    cutoff_trials 0
   end
 end
