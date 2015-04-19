ec9f4f20653b3396175d08c2644147cbd8220f5c
Dan Fingal-Surma <dfingal@gmail.com>
2015-04-19 01:40:22 -0700

    Elixir and Haskell style changes

--- envelopes.hs
+++ envelopes.hs
@@ -5,24 +5,24 @@
 
 import System.Random
 
-num_trials = 10000
-prior_lower_max = 100
-max_cutoff = 2 * prior_lower_max
+numTrials = 10000
+priorLowerMax = 100
+maxCutoff = 2 * priorLowerMax
 
 -- Returns the result of a single trial. envelope=0 means we picked the lower
 -- of the two envelopes, envelope=1 means we picked the higher. We switch if
 -- the value is below the cutoff.
-singleTrial envelope lower_value cutoff =
+singleTrial envelope lowerValue cutoff =
   if value >= cutoff then value else other
-  where higher_value = 2 * lower_value
+  where higherValue = 2 * lowerValue
         (value, other) = if envelope == 0 
-          then (lower_value, higher_value) 
-          else (higher_value, lower_value)
+          then (lowerValue, higherValue) 
+          else (higherValue, lowerValue)
 
 -- Runs multiple trials to do Monte Carlo approximation of the expected value.
 -- Surprisingly, a tail recursive version of this is much slower.
-multiTrial envelopes lower_values cutoff = total_expected_value / fromIntegral num_trials
-    where total_expected_value = fromIntegral (multiTrial' envelopes lower_values cutoff) 
+multiTrial envelopes lowerValues cutoff = totalExpectedValue / fromIntegral numTrials
+    where totalExpectedValue = fromIntegral (multiTrial' envelopes lowerValues cutoff) 
           multiTrial' [] [] _ = 0
           multiTrial' (e:es) (l:ls) cutoff = (singleTrial e l cutoff) + (multiTrial' es ls cutoff)
 
@@ -35,11 +35,11 @@ randomIntList bounds len = do
 -- Generates the Monte Carlo approximation of the expected value for each 
 -- possible cutoff value.
 cutoffTrials cutoff = do
-  if cutoff /= max_cutoff then do
-    envelopes <- randomIntList (0, 1) num_trials
-    lower_values <- randomIntList (0, prior_lower_max) num_trials
-    let expected_value = multiTrial envelopes lower_values cutoff
-    putStrLn ("cutoff=" ++ show cutoff ++ ", expected_value=" ++ show expected_value)
+  if cutoff /= maxCutoff then do
+    envelopes <- randomIntList (0, 1) numTrials
+    lowerValues <- randomIntList (0, priorLowerMax) numTrials
+    let expectedValue = multiTrial envelopes lowerValues cutoff
+    putStrLn $ "cutoff=" ++ show cutoff ++ ", expectedValue=" ++ show expectedValue
     cutoffTrials (cutoff + 1)
   else
     return ()
