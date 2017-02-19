5cb2295967be1f099bdebed4fe9e39314261fb12
Dan Fingal-Surma <dfingal@gmail.com>
2017-02-19 02:40:05 -0800

    Typescript version.

---
+++ envelopes.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env ts-node
+
+// $ brew install typescript
+// $ npm install -g ts-node
+// $ npm install -g typescript
+// $ echo '{"compilerOptions": {"noImplicitAny": true}}' > tsconfig.json
+
+const NUM_TRIALS = 10000;
+const PRIOR_LOWER_MAX = 100;
+
+// Runs a single trial where an envelope is chosen.  If the chosen envelope has
+// a value < cutoff, the function will switch envelopes, otherwise it will keep
+// the envelope it has chosen. Returns the value of the envelope it ultimately 
+// selects.
+function singleTrial(cutoff: number): number {
+  let lower_value = Math.random() * PRIOR_LOWER_MAX;
+  let higher_value = 2 * lower_value;
+  if (Math.random() < 0.5) {
+    return lower_value >= cutoff ? lower_value : higher_value;
+  } else {
+    return higher_value >= cutoff ? higher_value : lower_value;
+  }
+}
+
+// Runs many trials at a given cutoff to approximate the expected value.
+function multiTrial(cutoff: number): number {
+  let total = 0;
+  for (let i = 0; i < NUM_TRIALS; i++) {
+    total += singleTrial(cutoff);
+  }
+  return total / NUM_TRIALS;
+}
+
+for (let cutoff = 0; cutoff <= 2 * PRIOR_LOWER_MAX; cutoff++) {
+  console.log('cutoff=' + cutoff + ', expected_value=' + multiTrial(cutoff));
+}
