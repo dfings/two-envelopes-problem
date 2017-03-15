98f8c587e61ab45714ebae0654b6a7d5af48bbb1
Dan Fingal-Surma <dfingal@gmail.com>
2017-03-15 14:10:43 -0700

    Updated TS compile instructions.

--- envelopes.ts
+++ envelopes.ts
@@ -5,12 +5,17 @@
 // $ npm install -g typescript
 // $ echo '{"compilerOptions": {"noImplicitAny": true}}' > tsconfig.json
 
+// $ ./envelopes.ts
+// or
+// $ tsc envelopes.ts --outFile envelopes.ts.js
+// $ ./envelopes.ts.js
+
 const NUM_TRIALS = 10000;
 const PRIOR_LOWER_MAX = 100;
 
 // Runs a single trial where an envelope is chosen.  If the chosen envelope has
 // a value < cutoff, the function will switch envelopes, otherwise it will keep
-// the envelope it has chosen. Returns the value of the envelope it ultimately 
+// the envelope it has chosen. Returns the value of the envelope it ultimately
 // selects.
 function singleTrial(cutoff: number): number {
   let lower_value = Math.random() * PRIOR_LOWER_MAX;
