e6efac053af44488f7d20c8b493e7fcadc52bdd0
Dan Fingal-Surma <dfingal@gmail.com>
2017-05-17 16:26:00 -0700

    Block comments

--- envelopes.kt
+++ envelopes.kt
@@ -11,10 +11,12 @@ val NUM_TRIALS = 10000
 val PRIOR_LOWER_MAX = 100
 var random = Random()
 
-// Runs a single trial where an envelope is chosen.  If the chosen envelope has
-// a value < cutoff, the function will switch envelopes, otherwise it will keep
-// the envelope it has chosen. Returns the value of the envelope it ultimately 
-// selects.
+/**
+ * Runs a single trial where an envelope is chosen.  If the chosen envelope has
+ * a value < cutoff, the function will switch envelopes, otherwise it will keep
+ * the envelope it has chosen. Returns the value of the envelope it ultimately 
+ * selects.
+ */
 fun singleTrial(cutoff: Int): Double {
   val pick = {value: Double, other: Double -> if (value >= cutoff) value else other}
   val lowerValue = random.nextDouble() * PRIOR_LOWER_MAX
@@ -22,7 +24,7 @@ fun singleTrial(cutoff: Int): Double {
   return if (random.nextBoolean()) pick(lowerValue, higherValue) else pick(higherValue, lowerValue)
 }
 
-// Runs many trials at a given cutoff to approximate the expected value.
+/** Runs many trials at a given cutoff to approximate the expected value. */
 fun multiTrial(cutoff: Int): Double {
   var total = 0.0
   for (i in 0..NUM_TRIALS)
