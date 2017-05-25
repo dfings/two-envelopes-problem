d852d572f95f139f5ac9b10f35793c077d826a5c
Dan Fingal-Surma <dfingal@gmail.com>
2017-05-25 11:51:15 -0700

    More idiomatic Dart.

--- envelopes.dart
+++ envelopes.dart
@@ -5,36 +5,33 @@
 
 import 'dart:math';
 
-const int NUM_TRIALS = 10000;
-const int PRIOR_LOWER_MAX = 100;
-final Random rng = new Random();
+const NUM_TRIALS = 10000;
+const PRIOR_LOWER_MAX = 100;
+final random = new Random();
 
 // Runs a single trial where an envelope is chosen.  If the chosen envelope has
 // a value < cutoff, the function will switch envelopes, otherwise it will keep
 // the envelope it has chosen. Returns the value of the envelope it ultimately 
 // selects.
-double singleTrial(int cutoff) {
-  double lower_value = rng.nextDouble() * PRIOR_LOWER_MAX;
-  double higher_value = 2 * lower_value;
-  if (rng.nextBool()) {
-    return lower_value >= cutoff ? lower_value : higher_value;
-  } else {
-    return higher_value >= cutoff ? higher_value : lower_value;
-  }
+singleTrial(var cutoff) {
+  var pick = (value, other) => value >= cutoff ? value : other;
+  var lower_value = random.nextDouble() * PRIOR_LOWER_MAX;
+  var higher_value = 2 * lower_value;
+  return random.nextBool() ? pick(lower_value, higher_value) : pick(higher_value, lower_value);
 }
 
 // Runs many trials at a given cutoff to approximate the expected value.
-double multiTrial(int cutoff) {
-  double total = 0;
-  for (int i = 0; i < NUM_TRIALS; i++) {
+multiTrial(var cutoff) {
+  var total = 0;
+  for (var i = 0; i < NUM_TRIALS; i++) {
     total += singleTrial(cutoff);
   }
   return total / NUM_TRIALS;
 }
 
-void main() {
+main() {
   for (int cutoff = 0; cutoff <= 2 * PRIOR_LOWER_MAX; cutoff++) {
-    double expectedValue = multiTrial(cutoff);
+    var expectedValue = multiTrial(cutoff);
     print('cutoff=$cutoff, expected_value=$expectedValue');
   }
 }
