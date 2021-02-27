14a61e3c80f26a391f16a6d566ea0f1f4d4d386a
Dan Fingal-Surma <dfingal@gmail.com>
2021-02-27 15:28:54 -0800

    Apply formatter

--- envelopes.py
+++ envelopes.py
@@ -5,29 +5,30 @@ import random
 NUM_TRIALS = 10000
 PRIOR_LOWER_MAX = 100
 
-def single_trial(cutoff):
-  """Runs a single trial where an envelope is chosen.
 
-  If the chosen envelope has a value < cutoff, the function will switch 
-  envelopes, otherwise it will keep the envelope it has chosen. Returns
-  the value of the envelope it ultimately selects.
-  """
-  lower_value = random.random() * PRIOR_LOWER_MAX
-  higher_value = 2 * lower_value
-  if random.random() < 0.5:
-    return lower_value if lower_value >= cutoff else higher_value
-  else:
-    return higher_value if higher_value >= cutoff else lower_value
-	
-		
+def single_trial(cutoff):
+    """Runs a single trial where an envelope is chosen.
+
+    If the chosen envelope has a value < cutoff, the function will switch 
+    envelopes, otherwise it will keep the envelope it has chosen. Returns
+    the value of the envelope it ultimately selects.
+    """
+    lower_value = random.random() * PRIOR_LOWER_MAX
+    higher_value = 2 * lower_value
+    if random.random() < 0.5:
+        return lower_value if lower_value >= cutoff else higher_value
+    else:
+        return higher_value if higher_value >= cutoff else lower_value
+
+
 def multi_trial(cutoff):
-  """Runs many trials at a given cutoff to approximate the expected value."""
-  total = 0
-  for _ in range(NUM_TRIALS):
-    total += single_trial(cutoff)
-  return total / NUM_TRIALS
+    """Runs many trials at a given cutoff to approximate the expected value."""
+    total = 0
+    for _ in range(NUM_TRIALS):
+        total += single_trial(cutoff)
+    return total / NUM_TRIALS
 
 
 # Approximates the expected value for each integral cutoff value.
 for cutoff in range(2 * PRIOR_LOWER_MAX + 1):
-  print("cutoff={}, expected_value={}".format(cutoff, multi_trial(cutoff)))
+    print("cutoff={}, expected_value={}".format(cutoff, multi_trial(cutoff)))
--- envelopes_opt.py
+++ envelopes_opt.py
@@ -4,30 +4,32 @@ from __future__ import print_function
 import random
 
 NUM_TRIALS = 10000
-		
+
+
 def multi_trial(cutoff):
-  """Runs many trials at a given cutoff to approximate the expected value."""
-  randfloat = random.random  # Avoid . call in the inner loop.
-  total = 0
-  cutoff_scaled = cutoff / 100.0  # Avoid multiplication in the inner loop.
-  for _ in range(NUM_TRIALS):
-    lower_value = randfloat()
-    if randfloat() < 0.5:
-      if lower_value >= cutoff_scaled:
-        # Avoid computation of higher_value in this path.
-        total = total + lower_value  
-      else:
-        total = total + 2 * lower_value
-    else:
-      higher_value = 2 * lower_value
-      if higher_value >= cutoff:
-        total = total + higher_value
-      else:
-        total = total + lower_value        
-  return 100 * total / NUM_TRIALS
+    """Runs many trials at a given cutoff to approximate the expected value."""
+    randfloat = random.random  # Avoid . call in the inner loop.
+    total = 0
+    cutoff_scaled = cutoff / 100.0  # Avoid multiplication in the inner loop.
+    for _ in range(NUM_TRIALS):
+        lower_value = randfloat()
+        if randfloat() < 0.5:
+            if lower_value >= cutoff_scaled:
+                # Avoid computation of higher_value in this path.
+                total = total + lower_value
+            else:
+                total = total + 2 * lower_value
+        else:
+            higher_value = 2 * lower_value
+            if higher_value >= cutoff:
+                total = total + higher_value
+            else:
+                total = total + lower_value
+    return 100 * total / NUM_TRIALS
 
 
 if __name__ == '__main__':
-  """Approximates the expected value for each integral cutoff value."""
-  for cutoff in range(201):
-    print("cutoff={}, expected_value={}".format(cutoff, multi_trial(cutoff)))
+    """Approximates the expected value for each integral cutoff value."""
+    for cutoff in range(201):
+        print("cutoff={}, expected_value={}".format(
+            cutoff, multi_trial(cutoff)))
