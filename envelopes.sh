e25bc75653bd85095944eb8cbae0038b0d70e858
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-01 10:08:29 -0800

    Run more formatters

--- envelopes.sh
+++ envelopes.sh
@@ -4,27 +4,27 @@
 NUM_TRIALS=10000
 PRIOR_LOWER_MAX=100
 
-CUTOFF_MAX=$((2*PRIOR_LOWER_MAX-1))
-NUM_TRIALS_MAX=$((NUM_TRIALS-1))
+CUTOFF_MAX=$((2 * PRIOR_LOWER_MAX - 1))
+NUM_TRIALS_MAX=$((NUM_TRIALS - 1))
 for cutoff in $(seq 0 $CUTOFF_MAX); do
   total_result="0"
   # Runs many trials at a given cutoff to approximate the expected value.
   for _ in $(seq 0 $NUM_TRIALS_MAX); do
     # Runs a single trial where an envelope is chosen.  If the chosen envelope has
     # a value < cutoff, the function will switch envelopes, otherwise it will keep
-    # the envelope it has chosen. Returns the value of the envelope it ultimately 
+    # the envelope it has chosen. Returns the value of the envelope it ultimately
     # selects.
-    lower_envelope=$((RANDOM%PRIOR_LOWER_MAX))
-    higher_envelope=$((lower_envelope*2))
-    envelope=$((RANDOM%2))
+    lower_envelope=$((RANDOM % PRIOR_LOWER_MAX))
+    higher_envelope=$((lower_envelope * 2))
+    envelope=$((RANDOM % 2))
 
-    if [[ ( $envelope -eq 0 && $lower_envelope -gt $cutoff ) || 
-          ( $envelope -eq 1 && $higher_envelope -lt $cutoff ) ]]; then
-      total_result=$((total_result+lower_envelope))
+    if [[ ($envelope -eq 0 && $lower_envelope -gt $cutoff) || (\
+      $envelope -eq 1 && $higher_envelope -lt $cutoff) ]]; then
+      total_result=$((total_result + lower_envelope))
     else
-      total_result=$((total_result+higher_envelope))
-    fi  
+      total_result=$((total_result + higher_envelope))
+    fi
   done
-  expected_value=`echo "scale=5; $total_result / $NUM_TRIALS" | bc`
+  expected_value=$(echo "scale=5; $total_result / $NUM_TRIALS" | bc)
   printf "cutoff=%s, expected_value=%s\n" "$@" $cutoff $expected_value
 done
