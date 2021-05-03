00ffb43e2e88bceda995ddb909f1b183e198add4
Dan Fingal-Surma <dfingal@gmail.com>
2021-05-03 16:14:07 -0700

    Run formatter

--- envelopes.dart
+++ envelopes.dart
@@ -1,5 +1,4 @@
 #!/usr/bin/env dart
-
 // $ brew tap dart-lang/dart
 // $ brew install dart
 
@@ -11,20 +10,21 @@ final random = Random();
 
 // Runs a single trial where an envelope is chosen.  If the chosen envelope has
 // a value < cutoff, the function will switch envelopes, otherwise it will keep
-// the envelope it has chosen. Returns the value of the envelope it ultimately 
+// the envelope it has chosen. Returns the value of the envelope it ultimately
 // selects.
 double singleTrial(int cutoff) {
   var pick = (value, other) => value >= cutoff ? value : other;
   var lower_value = random.nextDouble() * PRIOR_LOWER_MAX;
   var higher_value = 2 * lower_value;
-  return random.nextBool() ? pick(lower_value, higher_value) : pick(higher_value, lower_value);
+  return random.nextBool()
+      ? pick(lower_value, higher_value)
+      : pick(higher_value, lower_value);
 }
 
 // Runs many trials at a given cutoff to approximate the expected value.
 double multiTrial(int cutoff) {
   var total = 0.0;
-  for (var i = 0; i < NUM_TRIALS; i++)
-    total += singleTrial(cutoff);
+  for (var i = 0; i < NUM_TRIALS; i++) total += singleTrial(cutoff);
   return total / NUM_TRIALS;
 }
 
