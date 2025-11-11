8b40d86b969d71cb0884921255371a9d80009aee
Dan Fingal-Surma <dfingal@gmail.com>
2025-11-11 02:06:06 -0800

    Update envelopes.ex

--- envelopes.ex
+++ envelopes.ex
@@ -3,40 +3,57 @@
 # or
 # $ iex
 # iex(1)> c("envelopes.ex")
-# iex(2)> Envelopes.main  
+# iex(2)> Envelopes.main
 
 defmodule Envelopes do
+  @moduledoc """
+  A simulation of the two envelopes problem.
+  """
 
   @prior_lower_max 100
+  @num_trials 10_000
   @cutoff_max 2 * @prior_lower_max
-  @num_trials 10000
 
-  # Returns the result of a single trial. We switch if the value is below the
-  # cutoff.
+  @doc """
+  Runs a single trial of the simulation.
+  """
+  @spec single_trial(number) :: number
   def single_trial(cutoff) do
-    lower_value = :rand.uniform() * @prior_lower_max
+    lower_value = :random.uniform() * @prior_lower_max
     higher_value = 2 * lower_value
-    case :rand.uniform(2) do
-      1 -> if lower_value >= cutoff do lower_value else higher_value end
-      2 -> if higher_value >= cutoff do higher_value else lower_value end
+
+    # You are presented with one of the envelopes at random.
+    [presented_value, other_value] = Enum.shuffle([lower_value, higher_value])
+
+    # You decide whether to switch based on the cutoff.
+    if presented_value >= cutoff do
+      presented_value # Keep the envelope
+    else
+      other_value # Switch to the other envelope
     end
   end
 
-  # Returns the total value of all of the trials for a given cutoff level.
-  def get_multi_trial_total(_, @num_trials, total), do: total
-  def get_multi_trial_total(cutoff, i, total) do
-    get_multi_trial_total(cutoff, i + 1, total + single_trial(cutoff))
+  @doc """
+  Recursively calculates the total value from multiple trials for a given cutoff.
+  This function is tail-recursive. It should be called with initial
+  accumulators, e.g., `multi_trial(cutoff, 0, 0)`.
+  """
+  @spec multi_trial(number, non_neg_integer, number) :: number
+  def multi_trial(_, @num_trials, total), do: total
+  def multi_trial(cutoff, i, total) do
+    multi_trial(cutoff, i + 1, total + single_trial(cutoff))
   end
 
-  # Prints the expected value for each possible cutoff.
-  def cutoff_trials(@cutoff_max) do end
-  def cutoff_trials(cutoff) do
-    expected_value = get_multi_trial_total(cutoff, 0, 0) / @num_trials
-    IO.puts :io_lib.format("cutoff=~w, expected_value=~w", [cutoff, expected_value])
-    cutoff_trials(cutoff + 1)
-  end
-
+  @doc """
+  Runs the simulation for a range of cutoff values and prints the expected
+  outcome for each cutoff.
+  """
   def main do
-    cutoff_trials(0)
+    0..@cutoff_max
+    |> Enum.each(fn cutoff ->
+      total_value = multi_trial(cutoff, 0, 0)
+      expected_value = total_value / @num_trials
+      IO.puts("cutoff=#{cutoff}, expected_value=#{expected_value}")
+    end)
   end
 end
